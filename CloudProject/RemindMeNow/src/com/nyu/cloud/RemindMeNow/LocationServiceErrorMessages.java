e68701df81baefbe287fb1d13e03f986389862c4
Amey Bhavsar <ameybhavsar@gmail.com>
2014-11-09 19:49:39 -0500

    Update LocationServiceErrorMessages.java

--- CloudProject/RemindMeNow/src/com/nyu/cloud/RemindMeNow/LocationServiceErrorMessages.java
+++ CloudProject/RemindMeNow/src/com/nyu/cloud/RemindMeNow/LocationServiceErrorMessages.java
@@ -15,9 +15,6 @@ package com.nyu.cloud.RemindMeNow;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
-
 import com.google.android.gms.common.ConnectionResult;
 import android.content.Context;
 import android.content.res.Resources;
